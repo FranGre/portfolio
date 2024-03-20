aff954f3811fd6a09125be6e62472396ebcf72e7
Fran Gregori Tandazo <frangregori61@gmail.com>
2024-03-20 20:28:27 +0100

    [MOD] - conocimientos añadidos y estilados

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,93 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import vueLogo from '/vue.svg'
+import laravelLogo from '/laravel.svg'
+import wordpressLogo from '/wordpress.svg'
+import prestashopLogo from '/prestashop.svg'
+import sassLogo from '/sass.svg'
+import cssLogo from '/css.svg'
+import jsLogo from '/js.svg'
+import tsLogo from '/ts.svg'
+import phpLogo from '/php.svg'
+import javaLogo from '/java.svg'
+import htmlLogo from '/html.svg'
+import gitLogo from '/git.svg'
+import dockerLogo from '/docker.svg'
+import linuxLogo from '/linux.svg'
+import vscodeLogo from '/vscode.svg'
+import phpstormLogo from '/phpstorm.svg'
+import intellijideaLogo from '/intellijidea.svg'
 import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <h1>Fran Gregori Tandazo</h1>
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <h2>Conocimientos</h2>
+        <a target="_blank">
+          <img src={vueLogo} className="logo vue" alt="Vue logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a target="_blank">
+          <img src={laravelLogo} className="logo laravel" alt="Laravel logo" />
+        </a>
+        <a target="_blank">
+          <img src={wordpressLogo} className="logo wordpress" alt="WordPress logo" />
+        </a>
+        <a target="_blank">
+          <img src={prestashopLogo} className="logo prestashop" alt="PrestaShop logo" />
+        </a>
+        <a target="_blank">
+          <img src={cssLogo} className="logo css" alt="CSS logo" />
+        </a>
+        <a target="_blank">
+          <img src={sassLogo} className="logo sass" alt="SASS logo" />
+        </a>
+        <a target="_blank">
+          <img src={jsLogo} className="logo js" alt="JavaScript logo" />
+        </a>
+        <a target="_blank">
+          <img src={tsLogo} className="logo ts" alt="TypeScript logo" />
+        </a>
+        <a target="_blank">
+          <img src={phpLogo} className="logo php" alt="PHP logo" />
+        </a>
+        <a target="_blank">
+          <img src={javaLogo} className="logo java" alt="Java logo" />
+        </a>
+        <a target="_blank">
+          <img src={htmlLogo} className="logo html" alt="HTML logo" />
+        </a>
+        <a target="_blank">
+          <img src={gitLogo} className="logo git" alt="Git logo" />
+        </a>
+        <a target="_blank">
+          <img src={dockerLogo} className="logo docker" alt="Docker logo" />
+        </a>
+        <a target="_blank">
+          <img src={linuxLogo} className="logo linux" alt="Linux logo" />
+        </a>
+        <a target="_blank">
+          <img src={viteLogo} className="logo vite" alt="Vite logo" />
+        </a>
+        <a target="_blank">
+          <img src={vscodeLogo} className="logo vscode" alt="VSCode logo" />
+        </a>
+        <a target="_blank">
+          <img src={phpstormLogo} className="logo phpstorm" alt="PhpStorm logo" />
+        </a>
+        <a target="_blank">
+          <img src={intellijideaLogo} className="logo intellijidea" alt="IntellijIdea logo" />
+        </a>
+
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
